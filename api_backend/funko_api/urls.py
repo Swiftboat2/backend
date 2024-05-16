a4a9539845ece750cdaf19442029af20cd557f2a
lautaropelozo <lautaropelozo@gmail.com>
2024-05-15 22:45:57 -0300

    json response

--- api_backend/funko_api/urls.py
+++ api_backend/funko_api/urls.py
@@ -4,5 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('about/', views.about, name='about'),
- 
+    path('funkos_rest/', views.funkos_rest, name='funkos_rest'),
 ]
--- api_backend/funko_api/views.py
+++ api_backend/funko_api/views.py
@@ -1,12 +1,21 @@
 from django.shortcuts import render
+from django.shortcuts import render
+from funko_api.models import Funko
+from django.http import JsonResponse
+from funko_api.serializers import FunkoSerializer
+ 
+def get_all_funkos():
+    funkos = Funko.objects.all().order_by('number')
+    funkos_serializers = FunkoSerializer(funkos, many=True)
+    return funkos_serializers.data
 
 def index(request):
-    context = {
-        'title': 'Home',
-        'heading': 'Bienvenido al home!',
-        'content': 'Contenido del home',
-    }
-    return render(request, 'index.html', context)
+    funkos = get_all_funkos()
+    return render(request, 'index.html', {'funkos': funkos})
+
+def funkos_rest(request):
+    funkos = get_all_funkos()
+    return JsonResponse(funkos, safe=False)
 
 def about(request):
     return render(request, 'about.html')
